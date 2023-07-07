a8b9492cb76d3d83233fa5aee6b559df1d648366
webowares1012 <116973598+webowares1012@users.noreply.github.com>
2023-07-07 10:35:40 +0530

    first commit

---
+++ models/gameData.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const gameDataSchema = new mongoose.Schema({
+    gameID: {
+        type:String,
+        require:true,
+    },
+    drawTime: {
+        type:String,
+        require:true,
+    },
+    currentRoundID: {
+        type:String,
+        require:true,
+    },
+    previousResults: [
+        {
+            dice1: Number,
+            dice2: Number,
+        }
+    ]
+});
+
+gameDataSchema.set('timestamps', true);
+
+const GameData = mongoose.model('gameData', gameDataSchema);
+module.exports = GameData;
